0182dd4da7c7033939db92e875323d966da5d63e
Eivind Brox <eivbro@protonmail.com>
2022-12-23 11:39:42 +0100

    Finnished day 7

--- day7/day7.py
+++ day7/day7.py
@@ -1,4 +1,4 @@
-with open('test_input.txt') as f:
+with open('input.txt') as f:
     lines = f.readlines()
 
 class dir:
@@ -6,25 +6,49 @@ class dir:
         self.name = name
         self.children_dirs = {}
         self.size = 0
+        self.parent = self
+        
+    def calculateSize(self):
+        for d in self.children_dirs:
+            self.size += self.children_dirs[d].calculateSize()
+        return self.size
+
+def day7_one():
+    size = 0
+    for d in dirs:
+        if d.size <= 100000:
+            size += d.size
+    print(size)
+
+def day7_two():
+    free = 7e7 - root.size
+    needed = 3e7
+    delete_at_least = needed - free
+    oldSmallest = dirs[0]
+    for d in dirs:
+        if d.size >= delete_at_least and d.size < oldSmallest.size:
+            oldSmallest = d
+    print(oldSmallest.size)
 
 root = dir("/")
-over = root
+current = root
+dirs = []
+dirs.append(current)
 for l in lines[1:]:
     split = l.split()
-    if split[0] == '$':
-        if split[0] == "dir":
-            over.children_dirs
-        elif split[1] == "cd":
+    if split[0] == "$":
+        if split[1] == "cd":
             if split[2] != "..":
-                over
-                dirs[dir_position].children_dirs.append(dirs[dir_position+1])
-                dir_position += 1
+                current = current.children_dirs[split[2]]
             else:
-                dir_position -= 1
-    elif split[0] != "dir" and split[0]:
-        dirs[dir_position].size += int(split[0])
+                current = current.parent
+    elif split[0] == "dir":
+        current.children_dirs[split[1]] = dir(split[1])
+        dirs.append(current.children_dirs[split[1]])
+        current.children_dirs[split[1]].parent = current
+    else:
+        current.size += int(split[0])
 
-dirnum = 0
-print(dirs[dirnum].name)
-print(dirs[dirnum].size)
-print(dirs[dirnum].children_dirs)
+root.calculateSize()
+
+day7_two()
